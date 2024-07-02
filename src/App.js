31481ac00abb248109c6c5f18594c880c6310802
jun0807 <adgjl0807@naver.com>
2024-07-02 20:24:31 +0900

    0702_2
    달러&코인 변환

--- src/App.js
+++ src/App.js
@@ -3,43 +3,50 @@ import { useEffect, useState } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [usd, setUsd] = useState(0);
-  const select = (event) => {
-    setUsd(event.target.value);
-  };
-  const onChange = (event) => {
-    setMoney(event.target.value);
-  };
+  const [selected, setSelected] = useState(0);
+  const [selectedUnit, setSelectedUnit] = useState('');
+  const [buyable, setBuyable] = useState(0);
+
   useEffect(() => {
     fetch('https://api.coinpaprika.com/v1/tickers')
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setSelected(json[0].quotes.USD.price);
+        setSelectedUnit(json[0].symbol);
       });
   }, []);
+
+  const handleSelect = (event) => {
+    const selectedValues = event.target.value.split(',');
+    setSelected(selectedValues[0]);
+    setSelectedUnit(selectedValues[1]);
+  };
+
+  const handleInput = (event) => {
+    setBuyable(event.target.value / selected);
+  };
+
   return (
     <div>
       <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      <input
-        onChange={onChange}
-        type='number'
-        value={money}
-        placeholder='write your money'
-      />
       {loading ? (
-        <strong>loading...</strong>
+        <strong>Loading...</strong>
       ) : (
-        <select onChange={select} value={coin.quotes.USD.price}>
+        <select onChange={handleSelect}>
           {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            <option key={coin.id} value={[coin.quotes.USD.price, coin.symbol]}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
-      {money > 0 ? `${usd}` : null}
+      <hr />
+      Budgets: <input onChange={handleInput}></input> USD
+      <br />
+      Buyable: <span>{buyable}</span> {selectedUnit}
+      <br />
     </div>
   );
 }
