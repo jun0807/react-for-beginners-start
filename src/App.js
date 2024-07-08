6d5c0f31db9490c68549f7f585f025c0fa04008b
jun0807 <adgjl0807@naver.com>
2024-07-08 20:38:22 +0900

    asdas

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
+        <Route path='/movie/:id' element={<Detail />}></Route>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
       </Routes>
     </Router>
